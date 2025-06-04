a38a30060dfc0b5f9176d6a2140b9a72527e99ef
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-04 18:10:40 +0530

    _

---
+++ basics/first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout<<"Hello";
+    cout << endl;
+    cout << endl;
+    cout<<"World";
+    cout << endl;
+    cout<<"This is my first C++ program";
+}
