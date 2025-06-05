e35f85920274b67f4b969e20235ad069cdea0103
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-05 13:26:40 +0530

    __

--- basics/first.cpp
+++ basics/first.cpp
@@ -12,5 +12,7 @@ int main (){
     // cout << endl;
     // cout<<"4"+3; // This line will cause an error because you cannot concatenate a string and an integer directly in C++
     cout << endl;
-    
+    int x=8;
+    cout<<x+1;
+
 }
