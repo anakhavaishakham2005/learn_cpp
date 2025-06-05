e091203c92115958cc8aa53ade44334ade817aba
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-05 13:03:16 +0530

    _

--- basics/first.cpp
+++ basics/first.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 using namespace std;
 int main (){
-    cout<<"Hello";
+    // cout<<"Hello";
+    // cout << endl;
+    // cout << endl;
+    // cout<<"World";
+    // cout << endl;
+    // cout<<"This is my first C++ program";
+    // cout <<endl;
+    // cout <<4+3;
+    // cout << endl;
+    // cout<<"4"+3; // This line will cause an error because you cannot concatenate a string and an integer directly in C++
     cout << endl;
-    cout << endl;
-    cout<<"World";
-    cout << endl;
-    cout<<"This is my first C++ program";
+    
 }
