7a29f79eec3a586c054626c834c150a1dd365831
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-07 12:18:56 +0530

    type

--- basics/typecast.cpp
+++ basics/typecast.cpp
@@ -4,5 +4,10 @@ int main(){
     char a= 'e';
     cout<< a << endl;
     cout<<(int)a << endl; // Typecast char to int
+    cout<<5/2<<endl;
+    cout<<5.0/2<<endl; // Implicit typecasting from int to float
+    cout<<5/2.0<<endl; // Implicit typecasting from int to float
+    cout<<5.0/2.0<<endl; // Implicit typecasting from int to float
+    cout<<5.0/2.0f<<endl; // Implicit typecasting from int to float
 
 }
