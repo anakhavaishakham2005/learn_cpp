71b39abd712f651455312202397bba4df169222a
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-07 11:55:36 +0530

    multiple_cin

--- basics/arithmetic,pre-post.cpp
+++ basics/arithmetic,pre-post.cpp
@@ -7,15 +7,23 @@ int main(){
     // cout<<"Multiplication: " << (x * y) << endl;
     // cout<<"Division: " << (float(x) / y) << endl;
     // cout<<"Modulus: " << (x % y) << endl;
-    int x=10;
-    cout << "Increment: " << (++x) << endl; // Pre-increment
-    cout<<x << endl; // Check value after pre-increment
-    cout << "Decrement: " << (--x) << endl; // Pre-decrement
-    cout<<x << endl; // Check value after pre-decrement
-    cout << "Post-increment: " << (x++) << endl; // Post-increment
-    cout<<x << endl; // Check value after post-increment
-    cout << "Post-decrement: " << (x--) << endl; // Post-decrement
-    cout<<x << endl; // Check value after post-decrement
+
+    // int x=10;
+    // cout << "Increment: " << (++x) << endl; // Pre-increment
+    // cout<<x << endl; // Check value after pre-increment
+    // cout << "Decrement: " << (--x) << endl; // Pre-decrement
+    // cout<<x << endl; // Check value after pre-decrement
+    // cout << "Post-increment: " << (x++) << endl; // Post-increment
+    // cout<<x << endl; // Check value after post-increment
+    // cout << "Post-decrement: " << (x--) << endl; // Post-decrement
+    // cout<<x << endl; // Check value after post-decrement
+
+    float r,h;
+    cout << "Enter radius and height of cylinder: ";
+    cin>> r >> h;
+    cout << "Volume of Cylinder:" << (22/7)*r*r*h << endl;
+    cout << "Surface area: " << 2*3.14*r *(h+r)<< endl;
+
 
 
 }
