a61f63ad5b96b97d7c5c0fce30f11c7c35da97d1
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-07 12:22:49 +0530

    type

--- basics/typecast.cpp
+++ basics/typecast.cpp
@@ -11,4 +11,8 @@ int main(){
     cout<<5.0/2.0f<<endl; // Implicit typecasting from int to float
     cout<<7/22*(3.14+2)*3/5<<endl;
     //here 7/22 is integer division, so it will be 0.
+    float d= 2/5;
+    cout << d << endl; // This will print 0.0 because 2/5 is integer division
+    float e= 2.0/5; // Explicit typecasting to float;
+    cout << e << endl; // This will print 0.4 because 2.0/5 is float division
 }
