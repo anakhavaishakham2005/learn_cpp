0cf88dfe5f5249e2000a2d14981d6dc1101b38c5
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-08 11:28:28 +0530

    prime

--- basics/conditionals.cpp
+++ basics/conditionals.cpp
@@ -1,18 +1,36 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,count=0;
-    cout << "Enter a number: ";
-    cin>>a;
-    for(int i=1;a/2>i;i++){
-        if(a%i==0){
-            count++;
-        }}
-        if(count==1 && a==a){
-            cout<<a<<" is a prime number."<<endl;
-        }
-        else{
-            cout<<a<<" is not a prime number."<<endl;
-        }
+    // int a,count=0;
+    // cout << "Enter a number: ";
+    // cin>>a;
+    // for(int i=1;a/2>i;i++){
+    //     if(a%i==0){
+    //         count++;
+    //     }}
+    //     if(count==1 && a==a){
+    //         cout<<a<<" is a prime number."<<endl;
+    //     }
+    //     else{
+    //         cout<<a<<" is not a prime number."<<endl;
+    //     }
+
+    int a,b,c;
+    cout<< "Enter age of Ram, Shyam and Mohan:" ;
+    cin>>a>>b>>c;
+    if(a<b and a<c){
+        cout<< "Ram is youngest";
+    }
+    else if(b<a and b<c){
+        cout<< "Shyam is youngest";
+    }
+    else if(c<a and c<b){
+        cout<< "Mohan is youngest";
+    }
+    else{
+        cout<< "All are of same age";
+    }
+
+    
     
 }
