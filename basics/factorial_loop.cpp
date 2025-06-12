a408db5d06dad532784c7c2730b2899418316776
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-12 12:10:22 +0530

    + sign

--- basics/factorial_loop.cpp
+++ basics/factorial_loop.cpp
@@ -21,14 +21,33 @@ int main(){
     //     cout << endl;
     // }
 
-    int i,j,n;
-    cin >> n;               //0
-    for(i=0;i<n;i++){       //1 0
-      for(j=0;j<i;j++){     //0 1 0
-          cout << "0 ";     //1 0 1 0
-        if((i+j)%2==0)
-          cout << "1 ";
-        else 
-          cout << "0 ";
+    // int i,j,n;
+    // cin >> n;               //0
+    // for(i=0;i<n;i++){       //1 0
+    //   for(j=0;j<i;j++){     //0 1 0
+    //       cout << "0 ";     //1 0 1 0
+    //     if((i+j)%2==0)
+    //       cout << "1 ";
+    //     else 
+    //       cout << "0 ";
+    // }cout << endl;
+    // }
+
+int i,j,n;
+    cin >> n;      
+    int mid=n/2+1;              //  *  
+    for(i=0;i<n;i++){           //  *  
+      for(j=0;j<n;j++){         //***** 
+          if(i==mid | j==mid)   //  *  
+              cout << "*";      //  *   
+          else cout << " ";     
     }cout << endl;
+    }
+
+
+
+
+
+    return 0;
+
 }
