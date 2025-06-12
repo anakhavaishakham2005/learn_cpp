fd37e5ee2d15e37afa026919a206a4d3c91c8678
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-12 11:49:44 +0530

    010 pattern

--- basics/factorial_loop.cpp
+++ basics/factorial_loop.cpp
@@ -11,14 +11,24 @@ int main(){
 
 
 
-    int i;
-    char j,n;
-    cin >> n;
-    for(i=int('a');i<int(n);i++){
-        for(j='a';j<=n;j++){
-            cout << char(j) << " ";
-        }
-        cout << endl;
-    }
+    // int i;
+    // char j,n;
+    // cin >> n;
+    // for(i=int('a');i<int(n);i++){
+    //     for(j='a';j<=n;j++){
+    //         cout << char(j) << " ";
+    //     }
+    //     cout << endl;
+    // }
 
+    int i,j,n;
+    cin >> n;               //0
+    for(i=0;i<n;i++){       //1 0
+      for(j=0;j<i;j++){     //0 1 0
+          cout << "0 ";     //1 0 1 0
+        if((i+j)%2==0)
+          cout << "1 ";
+        else 
+          cout << "0 ";
+    }cout << endl;
 }
