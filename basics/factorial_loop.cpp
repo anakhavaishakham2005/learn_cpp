9fe0f88f5acc00bb25d760046aa981aeac83ad0f
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-12 11:15:04 +0530

    alphabets

--- basics/factorial_loop.cpp
+++ basics/factorial_loop.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,n,j;
+    // int i,n,j;
+    // cin >> n;
+    // for(i=0;i<n;i++){
+    //     for(j=0;j<n;j++){
+    //         cout << j+1;
+    //     } cout << endl;
+    // }
+
+
+
+    int i;
+    char j,n;
     cin >> n;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cout << j+1 << " ";
+    for(i=int('a');i<int(n);i++){
+        for(j='a';j<=n;j++){
+            cout << char(j) << " ";
         }
         cout << endl;
     }
